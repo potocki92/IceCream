733cb7d0bf0e265919f222bdfe2d78e4229ee864
MateuszCharysz <charyszek@interia.pl>
2022-12-20 02:00:53 +0100

    rename for modal

--- src/js/modal-header-animation.js
+++ src/js/modal-header-animation.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[buy-modal-open]'),
+    closeModalBtn: document.querySelector('[buy-modal-close]'),
+    modal: document.querySelector('[buy-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
